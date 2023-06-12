547acb64ad4b271a167efaa5e9700cdc88d84c5b
Hank McGill <henrymcgill@gmail.com>
2023-06-12 18:46:18 -0400

    fresh restart

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import App from './app';
-
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-
-root.render(<App />);

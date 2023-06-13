cd3eeb8e9eb2f8909ca533484091ceb4e20efe83
Hank McGill <henrymcgill@gmail.com>
2023-06-12 21:06:54 -0400

    started laying out basic react components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Login from './components/Login';
+import Page from './components/Page';
 
 const App = () => {
-  return <h1>Hello world! I am using React</h1>
+  return(
+  <>
+  <h1>Hello world! I am using React</h1>
+  <Login />
+  -  - - - - - 
+  <Page />
+  </>)
 }
 
 export default App

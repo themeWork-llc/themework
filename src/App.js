e9306cd27dfb6fbaf98c56204e637eac40fe9381
Hank McGill <henrymcgill@gmail.com>
2023-06-12 21:12:08 -0400

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './app.css';
+
 import Login from './components/Login';
 import Page from './components/Page';
 
 const App = () => {
   return(
   <>
-  <h1>Hello world! I am using React</h1>
+  <h1 className="text-primary text-4xl font-bold">hello from react</h1>
   <Login />
   -  - - - - - 
   <Page />

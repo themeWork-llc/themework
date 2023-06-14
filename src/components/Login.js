2fc450d1f746b3248ff87ace55bb0d99f5f97782
waynetwleung <waynetwleung@gmail.com>
2023-06-13 20:05:57 -0400

    before merge to main to consolidate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import Create from './Create'
+import Join from './Join'
 
 export default function Login (props) {
+
+
+  
+
   return (
+    /*
+    3 senarios
+    create room
+    no id, no pw
+    join room
+    id, no pw
+    live room
+    id and pw
+    */
+
     <main>
+      <div>
+        {!props.room.id && !props.room.pw ? <Create />}
+        
+      </div>
       - - - - login - - - 
       <button onClick={props.handleLogin}>sign in</button>
       - - - - - - - - - - 
-      </main>
+    </main>
+
+    // <main>
+    //   - - - - login - - - 
+    //   <button onClick={props.handleLogin}>sign in</button>
+    //   - - - - - - - - - - 
+    //   </main>
   )
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -5,17 +5,27 @@ import EditorContainer from './EditorContainer'
 export default function Page () {
 
   const [loggedIn, setIsLoggedIn] = useState(false)
+  const [room, setRoom] = useState({})
   const handleLogin = () => setIsLoggedIn((current) => !current)
   
 
   return (
+    /*
+    3 senarios
+    create room
+    no id, no pw
+    join room
+    id, no pw
+    live room
+    id and pw
+    */
     <body>
     <h2>MAIN PAGE</h2>
     <section>
       <div>{loggedIn ? <>logged in!</> : <>logged out</>}</div>
       - - - - - - - - - - -
       {!loggedIn?
-      <Login handleLogin={handleLogin} /> :
+      <Login handleLogin={handleLogin} room={room}/> :
       <EditorContainer handleLogin={handleLogin}/>
       }
     </section>

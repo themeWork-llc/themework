a67644845825d3946e1432942a1fa08163ede3a4
waynetwleung <waynetwleung@gmail.com>
2023-06-14 16:35:04 -0400

    fixed jest mongo

--- __test__/mongoDb.test.js
+++ __test__/mongoDb.test.js
@@ -1,3 +1,5 @@
+global.TextDecoder = require('util').TextDecoder;
+global.TextEncoder = require('util').TextEncoder;
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
@@ -7,12 +9,12 @@ describe('insert', () => {
   let mongoServer;
 
   beforeAll(async () => {
-    mongoServer = new MongoMemoryServer();
-    const mongoUri = await mongoServer.getUri();
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
 
     connection = await MongoClient.connect(mongoUri, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
     db = await connection.db();
   });
@@ -20,16 +22,15 @@ describe('insert', () => {
   afterAll(async () => {
     await connection.close();
     await mongoServer.stop();
-    await db.close();
   });
 
   it('should insert a doc into collection', async () => {
     const rooms = db.collection('rooms');
-    
-    const mockRoom = {password: '12345678', text: 'hello world'};
+
+    const mockRoom = { password: '12345678', text: 'hello world' };
     await rooms.insertOne(mockRoom);
 
-    const insertedRoom = await rooms.findOne({password: '12345678'});
+    const insertedRoom = await rooms.findOne({ password: '12345678' });
     expect(insertedRoom).toEqual(mockRoom);
   });
 });

3f6fd0dd4987575631d2f7e3712c4b75a11e6d50
sjin-k <sjink@pm.me>
2023-06-15 09:47:12 -0400

    changing branch

--- src/components/EditorContainer.js
+++ src/components/EditorContainer.js
@@ -108,9 +108,9 @@ export default function EditorContainer(props) {
           placeholder="say hello..."
           autocomplete="xyz123"
         ></TextArea>
-        <Button onClick={handleSubmit}>
+        <button className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center' onClick={handleSubmit}>
           save
-        </Button>
+        </button>
       </Form>
     </Main>
   );
